c2eb1bd05d7fbd9d6dd621bd0ef0bd6c882a771e
greta <greta.pereda@gmail.com>
2023-05-03 21:54:59 -0600

    pruebas

--- api.js
+++ api.js
@@ -1,6 +1,10 @@
 let apiUrl = 'http://localhost:8000/'
 
-if (process.env.NODE_ENV ==='production' ) { //condicional, si la variable de entorno es production, es decir esta hosteada
- apiUrl = process.env.REACT_APP_API         //entonces va a ser REACT_APP_API , si esto no ocurre entoces sera http://localhost:8000/
+if (import.meta.env.NODE_ENV ==='production' ) { //process.env.NODE_ENV ==='production' --> ver asi era antes
+ apiUrl = import.meta.env.VITE_API           //apiUrl = process.env.REACT_APP_API --> ver asi era antes
 }
 export default apiUrl
+
+//condicional, si la variable de entorno es production, es decir esta hosteada
+//entonces va a ser REACT_APP_API , si esto no ocurre entoces sera http://localhost:8000/
+
--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import './App.css'
 
 import apiUrl from '../api'
 
+
 function App() {
+  
   const [count, setCount] = useState(0)
 
   console.log(apiUrl)
-  console.log(process.env.NODE_ENV)
+  console.log(import.meta.env.NODE_ENV)
 
   return (
     <>
-      <p className='font-bold text-3x1'>Hola claseeeee</p>
+      <p className='font-bold text-3x1'>Hola claseeeee!!!!</p>
     </>
   )
 }
